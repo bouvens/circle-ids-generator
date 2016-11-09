6ef9afbf810aca8ae52a8045d2ab3bc9af69ddc6
Alexander Demin <bouvens@gmail.com>
2016-11-09 18:34:48 +0600

    Limit added, refactored

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
 
-const PARAMS = {
-    module: 7,
-    generator: 3,
-};
-
 class LabeledInput extends Component {
     render() {
         return (
@@ -29,12 +24,34 @@ class ValueRow extends Component {
 
 class Output extends Component {
     render() {
-        const rows = this.props.values.map(function (value, i) {
+        let values = [];
+        let repeats = {};
+        let hasRepeats = false;
+
+        for (let i = 1; i < this.props.module; i += 1) {
+            const next = Math.pow(this.props.generator, i) % this.props.module;
+
+            if (repeats[next]) {
+                hasRepeats = true;
+            }
+            repeats[next] = true;
+
+            if (i < this.props.limit) {
+                values.push(next);
+            }
+            else {
+                break;
+            }
+        }
+
+        const rows = values.map(function (value, i) {
             return <ValueRow value={value} key={i}/>
         });
 
         return (
             <div>
+                {hasRepeats &&
+                    <div>Has repeats.</div>}
                 {rows}
             </div>
         );
@@ -45,29 +62,11 @@ class CircleIDsGenerator extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            module: this.props.params.module,
-            generator: this.props.params.generator
+            module: 7,
+            generator: 3,
+            limit: 1000,
         };
     }
-
-    getValues = () => {
-        let values = [];
-        let repeats = {};
-
-        for (let i = 1; i < this.state.module; i += 1) {
-            const next = Math.pow(this.state.generator, i) % this.state.module;
-
-            if (repeats[next]) {
-                values.push('repeat: ' + next);
-            }
-            else {
-                values.push(next);
-            }
-            repeats[next] = true;
-        }
-
-        return values;
-    };
 
     changeHandler = (event) => {
         this.setState({
@@ -78,9 +77,29 @@ class CircleIDsGenerator extends Component {
     render() {
         return (
             <form className="generator">
-                <LabeledInput id="module" label="Module" value={this.state.module} onChange={this.changeHandler}/>
-                <LabeledInput id="generator" label="Generator" value={this.state.generator} onChange={this.changeHandler}/>
-                <Output values={this.getValues()}/>
+                <LabeledInput
+                    id="module"
+                    label="Module"
+                    value={this.state.module}
+                    onChange={this.changeHandler}
+                />
+                <LabeledInput
+                    id="generator"
+                    label="Generator"
+                    value={this.state.generator}
+                    onChange={this.changeHandler}
+                />
+                <LabeledInput
+                    id="limit"
+                    label="Limit"
+                    value={this.state.limit}
+                    onChange={this.changeHandler}
+                />
+                <Output
+                    module={this.state.module}
+                    generator={this.state.generator}
+                    limit={this.state.limit}
+                />
             </form>
         );
     }
@@ -89,7 +108,7 @@ class CircleIDsGenerator extends Component {
 class App extends Component {
     render() {
         return (
-            <CircleIDsGenerator params={PARAMS}/>
+            <CircleIDsGenerator />
         );
     }
 }
